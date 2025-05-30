7cb67a8af8e867780347fc9450cb5687da82fd5d
Yasin <wytm97@protonmail.com>
2025-05-30 15:35:08 +0200

    feat: add metadata filter component

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,17 +1,15 @@
-import { GrLinkNext } from "react-icons/gr";
-
-export default function ProjectCard({ title, children, href="#" }) {
+export default function ProjectCard({project, title, children}) {
     return (
-        <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-            <div className="px-4 py-5 sm:px-6">
-                <h1 className="font-bold text-xl">{title}</h1>
+        <li className="break-inside-avoid mb-4"
+            data-keywords={project.keywords.join(',')}
+            data-category={project.category}
+            data-status={project.status}>
+            <div className="overflow-hidden bg-white shadow-lg sm:rounded-lg">
+                <div className="px-4 py-5 sm:p-6">
+                    <h1 className="font-bold text-xl">{title}</h1>
+                    <div className="mt-2">{children}</div>
+                </div>
             </div>
-            <div className="px-4 py-5 sm:p-6">
-                {children}
-            </div>
-            <div className="px-4 py-4 sm:px-6 flex flex-row-reverse">
-                <a href={href} className="text-blue-600 flex gap-x-1 items-center">Read more <GrLinkNext className="p-0 m-0" /></a>
-            </div>
-        </div>
+        </li>
     )
 }
