d0a68c45ce72dc964697799c9d3336f2e2e145fb
Korbinian Bösl <korbinian.bosl@uib.no>
2025-08-27 15:31:20 +0200

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -115,10 +115,9 @@ const Links = () => {
         <div
             className="col-span-full lg:col-span-1 text-right place-content-end place-items-end place-self-center lg:place-self-end">
             <div role="list" className="flex sm:flex-row lg:flex-col gap-y-2 gap-x-2 [&_*]:text-gray-800">
-                <a href="#" className="text-sm leading-6">Helpdesk</a>
-                <a href="#" className="text-sm leading-6">Careers</a>
-                <a href="#" className="text-sm leading-6">Organizations</a>
-                <a href="#" className="text-sm leading-6">Sitemap</a>
+                <a href="research-support" className="text-sm leading-6">Support </a>
+                <a href="https://elixir-europe.org/about-us/vacancies" className="text-sm leading-6">Careers</a>
+                <a href="about" className="text-sm leading-6">Organizations</a>
             </div>
         </div>
     );
