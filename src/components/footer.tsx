3efa4b37981d6ec91f467a3a960770adc0f7f17c
Korbinian Bösl <korbinian.bosl@uib.no>
2025-08-27 15:25:18 +0200

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -129,36 +129,6 @@ const Links = () => {
     );
 };
 
-const NewsletterSubscription = () => {
-    return (
-        <div className="mt-4 border-t border-gray-900/10 lg:flex lg:items-center lg:justify-between">
-            <div className="mt-6">
-                <h3 className="text-base font-semibold leading-6">
-                    Subscribe to our newsletter
-                </h3>
-                <p className="text-sm">
-                    Get the latest news, articles, and resources
-                    delivered straight to your inbox.
-                </p>
-            </div>
-            <form className="mt-6 sm:flex sm:max-w-md">
-                <label htmlFor="email-address" className="sr-only">Email address</label>
-                <input type="email"
-                       name="email-address"
-                       id="email-address"
-                       autoComplete="email"
-                       required
-                       className="w-full min-w-0 appearance-none rounded-md border-0 bg-slate-50 dark:bg-dark-surface px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand-primary sm:w-56 sm:text-sm sm:leading-6"
-                       placeholder="Enter your email"/>
-                <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-                    <button type="submit"
-                            className="flex w-full items-center justify-center rounded-md bg-brand-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-brand-primary hover:saturate-150 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-primary">Subscribe
-                    </button>
-                </div>
-            </form>
-        </div>
-    );
-};
 
 const SocialButton = ({ name, link, icon }) => {
     return (
